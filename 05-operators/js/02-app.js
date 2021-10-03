e6d2b9f2a3d85c566996f6c1461b6acf2ccbffef
iOznny <f000170014@gmail.com>
2021-10-03 16:51:05 -0500

    Null and undefined

---
+++ 05-operators/js/02-app.js
@@ -0,0 +1,11 @@
+// Null and Undefined
+
+// Undefined
+let number;
+console.log(number);
+console.log(typeof number);
+
+// Null
+let number2 = null;
+console.log(number2);
+console.log(typeof number2);
