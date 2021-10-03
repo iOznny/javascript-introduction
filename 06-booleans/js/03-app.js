878fb83318e760636b58a7cebbf3f4c4dc9c365a
iOznny <f000170014@gmail.com>
2021-10-03 17:03:45 -0500

    Booleans

---
+++ 06-booleans/js/03-app.js
@@ -0,0 +1,10 @@
+const auth = true;
+
+if (auth) {
+    console.log('Si puedes ver Netflix');
+} else {
+    console.log('No puedes ver Netflix');
+}
+
+// Ternary operator
+console.log('Ternary operator -> ', auth ? 'Si puedes ver Netflix' : 'No puedes ver Netflix');
