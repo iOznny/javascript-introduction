21cfb5624032a19e2abf8412b347f962cb0bd5dc
iOznny <f000170014@gmail.com>
2021-10-03 17:00:42 -0500

    Booleans

--- 05-operators/js/02-app.js
+++ 05-operators/js/02-app.js
@@ -3,9 +3,12 @@
 // Undefined
 let number;
 console.log(number);
-console.log(typeof number);
+//console.log(typeof number);
 
 // Null
 let number2 = null;
 console.log(number2);
-console.log(typeof number2);
+//console.log(typeof number2);
+
+console.log(number == number2);
+console.log(number === number2);
---
+++ 06-booleans/js/01-app.js
@@ -0,0 +1,10 @@
+// Booleans
+
+const boolean1 = true;
+const boolean2 = false;
+const boolean3 = "true";
+
+console.log(typeof boolean1);
+console.log(typeof boolean2);
+console.log(boolean3);
+console.log(boolean1 == boolean3);
