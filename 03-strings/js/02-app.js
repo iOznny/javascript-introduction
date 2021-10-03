c7a97a3dafb4b7b1bfeb91cbb285f7d49e253d54
iOznny <f000170014@gmail.com>
2021-10-02 19:42:15 -0500

    Strings

---
+++ 03-strings/js/02-app.js
@@ -0,0 +1,14 @@
+// Methods and Functions
+
+const tweet = 'Monitor 20 inches';
+//console.log(tweet);
+
+// property length:  Know the length of the text string
+//console.log('length' + tweet.length);
+
+// Method indexOf: El método indexOf() retorna el primer índice en el que se puede encontrar 
+// un elemento dado en el array, ó retorna -1 si el elemento no esta presente.
+//console.log('indexOf' + tweet.indexOf('20'));
+
+// Method Includes
+//console.log('includes' + tweet.includes('Monitor'));
---
+++ 03-strings/js/03-app.js
@@ -0,0 +1,15 @@
+// Concatenation
+
+const product03 = ' Monitor 20 inches ';
+const price03 = '30 USD';
+
+// Join strings
+//console.log('Concat 03 ->' + product03.concat(price03));
+//console.log('Concat 03 ->' + product03.concat('Discount'));
+
+// Concat Examples
+//console.log('Concat 03 ->' + product03 + price03);
+//console.log('Concat 03 ->' + product03 + 'Hola' + price03);
+
+// Backtick ES6
+//console.log('Concat 03 ->' + ` El producto ${ product03 } tiene un precio de ${ price03 }`);
