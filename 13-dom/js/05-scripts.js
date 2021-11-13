85867c76b48687a04f732d63732a5e892533bb47
iOznny <f000170014@gmail.com>
2021-11-12 22:15:53 -0600

    changes

---
+++ 13-dom/js/05-scripts.js
@@ -0,0 +1,11 @@
+// Query selector all
+const card = document.querySelectorAll('.card');
+console.log(card);
+
+const forms = document.querySelectorAll('#formulario');
+console.log(forms);
+
+// If dont element exist
+const notExist = document.querySelectorAll('not-exist');
+console.log(notExist);
+
