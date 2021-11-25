af955d1329063463b7c954ea1e9a6c56daf97edb
iOznny <f000170014@gmail.com>
2021-11-25 17:54:26 -0600

    changes

---
+++ 14-events/js/01-scripts.js
@@ -0,0 +1,9 @@
+console.log(1);
+
+// Wait for the document to be ready
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Document ready!');
+    console.log(2);
+});
+
+console.log(3);
---
+++ 14-events/js/02-scripts.js
@@ -0,0 +1,35 @@
+// Events with mouse
+
+const nav = document.querySelector('.navegacion');
+
+// Register a event
+// nav.addEventListener('click', () => {
+//     console.log('Click in nav');
+// });
+
+// Mouseenter
+// nav.addEventListener('mouseenter', () => {
+//     console.log('Mouse enter in nav');
+//     nav.style.backgroundColor = 'transparent';
+// });
+
+// Mouseout
+// nav.addEventListener('mouseout', () => {
+//     console.log('Mouse out in nav');
+//     nav.style.backgroundColor = 'white';
+// });
+
+// Mousedown
+// nav.addEventListener('mousedown', () => {
+//     console.log('Mouse down');
+// });
+
+// Mouseup
+// nav.addEventListener('mouseup', () => {
+//     console.log('Mouse up');
+// });
+
+// DBL Click
+// nav.addEventListener('dblclick', () => {    
+//     console.log('Double Click in nav');
+// });
